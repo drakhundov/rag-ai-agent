87575c6daa41706b2ed7150154002b6dfebb12d7
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-08-16 16:48:43 +0400

    Refactored configuration functionality for safety and efficiency. It's been made more deterministic and rigid.

--- src/config.py
+++ src/config.py
@@ -1,35 +1,76 @@
 """This module deals with .env (API TOKENS) and configuration (models, paths)."""
 
-import os, dotenv, json, re
+import os, dotenv, json
+from dataclasses import dataclass
+from functools import lru_cache
+from pathlib import Path
+from typing import Dict, List
+
+from utils import replace_placeholders
 
 
+@dataclass(frozen=True)
+class Models:
+    chat_model_name: str
+    emb_model_name: str
+
+
+@dataclass(frozen=True)
+class Paths:
+    proj_dir: Path
+    cache_dir: Path
+    chroma_index_dir: Path
+    router_url: str
+
+
+@dataclass(frozen=True)
 class Config:
-    def __init__(self):
-        pass
+    openai_api_key: str
+    hf_token: str
+    models: Models
+    paths: Paths
+    prompt_templ: Dict[str:List, str:str]
 
-    @staticmethod
-    def load_conf() -> "Config":
-        conf = Config()
-        # Load variables from the '.env' file.
-        dotenv.load_dotenv()
 
-        conf.HF_TOKEN = os.getenv("HF_TOKEN")
-        if (OPENAI_API_KEY := os.getenv("OPENAI_API_KEY")) is None:
-            raise ValueError("You must define an OPENAI_API_KEY in the `.env` file.")
-        else:
-            conf.OPENAI_API_KEY = OPENAI_API_KEY
-        conf.PROJ_DIR = os.path.dirname(os.path.dirname(__file__))
-        with open("settings.json") as fp:
-            settings = json.load(fp)
-        for k, v in settings.items():
-            v = conf.replace_placeholders(v)
-            setattr(conf, k, v)
-        return conf
+# `lru_cache` caches function output values, so that
+# next time the function is called with the same 
+# arguments, the value is returned from cache.
+# `maxsize=1` tells to only store one args-return_val pair.
+@lru_cache(maxsize=1)
+def load_conf() -> Config:
+    # This file's path: PROJ_DIR/src/config.py
+    # Thus, we need the second parent.
+    proj_dir = Path(__file__).resolve().parents[1]
+    # Load variables from the '.env' file.
+    if not os.path.exists(proj_dir / ".env"):
+        raise FileNotFoundError(f"File `{proj_dir / '.env'}` does not exist.")
+    dotenv.load_dotenv(proj_dir / ".env", override=False)
 
-    def replace_placeholders(self, string: str) -> None:
-        pattern = r"\$\{(\w+)\}"
-        def callback(_match: str):
-            var_name = _match.group(1)
-            return getattr(self, var_name, f"${{{var_name}}}")
-        return re.sub(pattern, callback, string)
-    
+    hf_token = os.getenv("HF_TOKEN")
+    if (openai_api_key := os.getenv("OPENAI_API_KEY")) is None:
+        raise ValueError("You must define an OPENAI_API_KEY in the `.env` file.")
+    if not os.path.exists(proj_dir / "settings.json"):
+        raise FileNotFoundError(f"File `{proj_dir / 'settings.json'}` does not exist.")
+    with open(proj_dir / "settings.json") as fp:
+        settings = json.load(fp)
+    resolved = {
+        k: replace_placeholders(v, settings) if isinstance(v, str) else v
+        for k, v in settings.items()
+    }
+    paths = Paths(
+        proj_dir=proj_dir,
+        cache_dir=Path(resolved["CACHE_DIR"]).resolve(),
+        chroma_index_dir=Path(resolved["CHROMA_INDEX_DIR"]).resolve(),
+        router_url=resolved["ROUTER_URL"],
+    )
+    models = Models(
+        chat_model_name=resolved["MODELS"]["DEFAULT_CHAT_MODEL"],
+        emb_model_name=resolved["MODELS"]["DEFAULT_EMB_MODEL"],
+    )
+    return Config(
+        openai_api_key=openai_api_key,
+        hf_token=hf_token,
+        models=models,
+        paths=paths,
+        prompt_templ=resolved["PROMPT_TEMPL"],
+    )
