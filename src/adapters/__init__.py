897481a3c0cdd4067a67d02f4e1fdd3e7583953f
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-08-17 16:29:06 +0400

    Turned adapters into a module for easier importing.

---
+++ src/adapters/__init__.py
@@ -0,0 +1,5 @@
+from llm import ChatOpenAILLMService
+from retrieving import ChromaRetriever
+from text_splitting import SemanticTextSplitter
+
+__all__ = ["ChatOpenAILLMService", "ChromaRetriever", "SemanticTextSplitter"]
