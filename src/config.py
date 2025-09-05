727ecda1045b7969865451b018939d9b133ba0d6
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-09-05 22:57:08 +0400

    Added a prompt template for multi-query retrieval to the configuration and settings.

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import os, dotenv, json
 from dataclasses import dataclass
 from functools import lru_cache
 from pathlib import Path
-from typing import Dict, List
+from typing import List
 
 from utils.string import replace_placeholders
 
@@ -13,6 +13,12 @@ from utils.string import replace_placeholders
 class PromptTempl:
     input_variables: List[str]
     template: str
+
+
+@dataclass(frozen=True)
+class PromptTempls:
+    system: PromptTempl
+    multi_query_rag_prompt: PromptTempl
 
 
 @dataclass(frozen=True)
@@ -27,15 +33,17 @@ class Paths:
     cache_dir: Path
     chroma_index_dir: Path
     router_url: str
+    langsmith_api_url: str
 
 
 @dataclass(frozen=True)
 class Config:
     openai_api_key: str
     hf_token: str
+    langchain_api_key: str
     models: Models
     paths: Paths
-    prompt_templ: Dict[str:List, str:str]
+    prompt_templs: PromptTempls
     concat_bufsz: int
     breakpoint_percentile_threshold: int
 
@@ -54,9 +62,13 @@ def load_conf() -> Config:
         raise FileNotFoundError(f"File `{proj_dir / '.env'}` does not exist.")
     dotenv.load_dotenv(proj_dir / ".env", override=False)
 
-    hf_token = os.getenv("HF_TOKEN")
+    if (hf_token := os.getenv("HF_TOKEN")) is None:
+        raise ValueError("You must define an HF_TOKEN in the `.env` file.")
     if (openai_api_key := os.getenv("OPENAI_API_KEY")) is None:
         raise ValueError("You must define an OPENAI_API_KEY in the `.env` file.")
+    if (langchain_api_key := os.getenv("LANGCHAIN_API_KEY")) is None:
+        raise ValueError("You must define an LANGCHAIN_API_KEY in the `.env` file.")
+
     if not os.path.exists(proj_dir / "settings.json"):
         raise FileNotFoundError(f"File `{proj_dir / 'settings.json'}` does not exist.")
     with open(proj_dir / "settings.json") as fp:
@@ -70,21 +82,29 @@ def load_conf() -> Config:
         cache_dir=Path(resolved["CACHE_DIR"]).resolve(),
         chroma_index_dir=Path(resolved["CHROMA_INDEX_DIR"]).resolve(),
         router_url=resolved["ROUTER_URL"],
+        langsmith_api_url=resolved["LANGSMITH_API_URL"]
     )
     models = Models(
         chat_model_name=resolved["MODELS"]["DEFAULT_CHAT_MODEL"],
-        emb_model_name=resolved["MODELS"]["DEFAULT_EMB_MODEL"],
+        emb_model_name=resolved["MODELS"]["DEFAULT_EMB_MODEL"]
     )
-    prompt_templ = PromptTempl(
-        input_variables=resolved["PROMPT_TEMPL"]["INPUT_VARIABLES"],
-        template=resolved["PROMPT_TEMPL"]["TEMPLATE"],
+    prompt_templs = PromptTempls(
+        system=PromptTempl(
+            input_variables=resolved["PROMPT_TEMPL"]["INPUT_VARIABLES"],
+            template=resolved["PROMPT_TEMPL"]["TEMPLATE"],
+        ),
+        multi_query_rag_prompt=PromptTempl(
+            input_variables=resolved["MULTI_QUERY_RAG_PROMPT"]["INPUT_VARIABLES"],
+            template=resolved["MULTI_QUERY_RAG_PROMPT"]["TEMPLATE"],
+        )
     )
     return Config(
         openai_api_key=openai_api_key,
         hf_token=hf_token,
+        langchain_api_key=langchain_api_key,
         models=models,
         paths=paths,
-        prompt_templ=prompt_templ,
+        prompt_templs=prompt_templs,
         concat_bufsz=resolved["SENTENCE_CONCAT_BUFSZ"],
-        breakpoint_percentile_threshold=resolved["BREAKPOINT_PERCENTILE_THRESHOLD"],
+        breakpoint_percentile_threshold=resolved["BREAKPOINT_PERCENTILE_THRESHOLD"]
     )
