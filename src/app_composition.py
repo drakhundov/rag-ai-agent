d85d58d3e39b2a11d79d68fb2c3766ccac8be428
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-09-05 22:57:08 +0400

    Added a prompt template for multi-query retrieval to the configuration and settings. Also, using langsmith from now on.

--- src/app_composition.py
+++ src/app_composition.py
@@ -1,4 +1,4 @@
-import os, sys
+import os
 
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
@@ -9,11 +9,17 @@ from adapters import ChatOpenAILLMService, ChromaRetriever, SemanticTextSplitter
 from services.retriever_service import RetrieverService
 
 # Ensure imports could omit "src".
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
+# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 def build_app() -> RetrieverService:
     conf = load_conf()
+    # Set up LangSmith.
+    os.environ["LANGCHAIN_TRACING_V2"] = "true"
+    os.environ["LANGCHAIN_API_KEY"] = conf.langchain_api_key.get_secret_value()
+    os.environ["LANGCHAIN_PROJECT"] = "RAG"
+    os.environ["LANGSMITH_ENDPOINT"] = conf.paths.langsmith_api_url
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
     retriever = ChromaRetriever(
         pdf_paths=["context.pdf"],
         emb_model=HuggingFaceEmbeddings(model_name=conf.models.emb_model_name),
@@ -21,10 +27,10 @@ def build_app() -> RetrieverService:
         # Use default chroma index directory.
     )
     llm_model = ChatOpenAILLMService()  # Use default config.
-    prompt_template = PromptTemplate(
-        input_variables=conf.prompt_templ.input_variables,
-        template=conf.prompt_templ.template,
+    sys_prompt_template = PromptTemplate(
+        input_variables=conf.prompt_templs.system.input_variables,
+        template=conf.prompt_templs.system.template,
     )
     return RetrieverService(
-        retriever=retriever, llm_model=llm_model, prompt_template=prompt_template
+        retriever=retriever, llm_model=llm_model, sys_prompt_template=sys_prompt_template
     )
