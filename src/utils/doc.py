97471a1e35a157fb7ea7b03f8933eb35d2dc11f9
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-09-11 16:12:32 +0400

    Add module with utility functions for dealing with documents. Add function to retrieve unique documents from a list.

---
+++ src/utils/doc.py
@@ -0,0 +1,10 @@
+from typing import List
+from json import dumps, loads
+
+from langchain_core.documents import Document
+
+
+def get_unique_union(docs: List[Document]) -> List[Document]:
+    flattened = [dumps(doc) for doc in docs]
+    unique_docs = list(set(flattened))
+    return [loads(doc) for doc in unique_docs]
