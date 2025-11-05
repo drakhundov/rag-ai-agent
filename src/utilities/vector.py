acd434673c5ace6a0483f4fe2a56af05c18ae85f
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-11-05 21:24:26 +0300

    Cache embeddings for all texts.

--- src/utilities/vector.py
+++ src/utilities/vector.py
@@ -1,9 +1,12 @@
 from typing import List
+import hashlib
 
 import numpy as np
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
 
 from core.config import load_conf
+from core.types import CacheAttr
+from services.CacheManager import CacheManager
 
 conf = load_conf()
 
@@ -36,6 +39,23 @@ def embed_texts(texts: List[str], model_name: str = None) -> np.ndarray:
     """
     if not texts:
         return np.empty((0,))
+    embs = []
+    cache_hit_idx = set()
+    cmng = CacheManager("texts")
+    for i, text in enumerate(texts):
+        hashval = hashlib.md5(text.encode()).hexdigest()
+        hit, cached_emb = cmng.get(
+            cache_id=hashval,
+            attr=CacheAttr.EMBEDDINGS,
+            read_as_binary=True
+        )
+        if hit:
+            cache_hit_idx.add(i)
+            embs[i] = cached_emb
+    # If all texts have been retrieved from cache, return cached data.
+    if len(cache_hit_idx) == len(texts):
+        return np.array(embs, dtype=np.float32)
+    # Otherwise, run the encoder for some texts.
     if model_name is None:
         # Use the default embedding model if not specified.
         model_name = conf.models.emb_model_name
@@ -43,5 +63,9 @@ def embed_texts(texts: List[str], model_name: str = None) -> np.ndarray:
         model=model_name,
         huggingfacehub_api_token=conf.hf_token.get_secret_value()
     )
+    for i, text in enumerate(texts):
+        if i in cache_hit_idx:
+            continue
+        embs[i] = encoder.embed_query(text)
     embs = encoder.embed_documents(texts)
     return np.array(embs, dtype=np.float32)
